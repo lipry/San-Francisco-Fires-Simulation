Implemented variance reduction technique Implemented sample mean and sample variance technique Plotted mean intervention time vs. number of intervention unit graph General bug fix

simulation_package/discrete_events.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 
 from simulation_package.exponential import Exponential
+from simulation_package.normal import Normal
 from simulation_package.GeoCoordSimulator import GeoCoordSimulator
 
 
@@ -12,6 +13,13 @@ class SFFireSimulation:
         self.N_servers = n_server
         self.simulation_days = days
         self.df = sf_fires_df
+        self.fires_exp = Exponential(0.0009517109298514755)
+        self.server_service_exp = Normal(mu=8.507317993991224, variance=1.3689861848759046, t='bm', log=True)
+        self.primary = self.df['Action Taken Primary'].value_counts(normalize=True)
+        self.secondary = self.df['Action Taken Secondary'].value_counts(normalize=True)
+        self.other = self.df['Action Taken Other'].value_counts(normalize=True)
+        self.location_df = self.df[['Longitude', 'Latitude']].dropna()
+        self.gcs = GeoCoordSimulator(self.location_df)
 
     @staticmethod
     def timefseconds(seconds):
@@ -30,17 +38,10 @@ class SFFireSimulation:
     def get_min_time_server(times):
         return min(enumerate(times), key=operator.itemgetter(1))
 
-    #VALUTAARE PERMUTAZIONI AL POSTO CHE RAND
+    # VALUTAARE PERMUTAZIONI AL POSTO CHE RAND
     def simulate(self):
         #init utility variables
-        fires_exp = Exponential(1 / 1050.73914786)
-        server_service_exp = Exponential(1 / 1050.7392199)
-        primary = self.df['Action Taken Primary'].value_counts(normalize=True)
-        secondary = self.df['Action Taken Secondary'].value_counts(normalize=True)
-        other = self.df['Action Taken Other'].value_counts(normalize=True)
-        location_df = self.df[['Longitude', 'Latitude']].dropna()
-        gcs = GeoCoordSimulator(location_df)
-        gcs.calculate_clusters()
+        self.gcs.calculate_clusters()
         # init variables
         # Time variable
         t = 0
@@ -61,21 +62,23 @@ class SFFireSimulation:
         # Event list
         Tserver = [math.inf for _ in range(self.N_servers)]
 
-        Ta = fires_exp.random()
+        u = np.random.uniform(0, 1)
+        Ta = self.fires_exp.random(u)
         while t < self.simulation_days*86400:
             if Ta <= min(Ta, *Tserver):
                 # the next events is a fire
                 t = Ta
                 Na += 1
-                Ta = t + fires_exp.random()
+                u = np.random.uniform(0, 1)
+                Ta = t + self.fires_exp.random(u)
 
-                primary_action = np.random.choice(primary.index, 1, p=primary)
-                secondary_action = np.random.choice(secondary.index, 1, p=secondary)
-                other_actions = np.random.choice(other.index, 1, p=other)
-                coord = gcs.get_random_coordinates(1)
+                primary_action = np.random.choice(self.primary.index, 1, p=self.primary)
+                secondary_action = np.random.choice(self.secondary.index, 1, p=self.secondary)
+                other_actions = np.random.choice(self.other.index, 1, p=self.other)
+                coord = self.gcs.get_random_coordinates(1)
 
-                simulated_fire = simulated_fire.append({'Latitude': coord[0,0],
-                                                        'Longitude': coord[0,1],
+                simulated_fire = simulated_fire.append({'Latitude': coord[0, 0],
+                                                        'Longitude': coord[0, 1],
                                                         'Action Taken Primary': primary_action[0],
                                                         'Action Taken Secondary': secondary_action[0],
                                                         'Action Taken Other': other_actions[0]}, ignore_index=True)
@@ -84,7 +87,7 @@ class SFFireSimulation:
                 if n_live_fires <= self.N_servers:
                     i = self.find_first_empty_server(SS)
                     SS[i] = Na
-                    Tserver[i] = t + server_service_exp.random()
+                    Tserver[i] = t + self.server_service_exp.random(0)
             else:
                 # the next events is an intervention end
                 min_time_ix, min_time = self.get_min_time_server(Tserver)
@@ -96,7 +99,7 @@ class SFFireSimulation:
                     Tserver[min_time_ix] = math.inf
                 else:
                     SS[min_time_ix] += 1
-                    Tserver[min_time_ix] = t + server_service_exp.random()
+                    Tserver[min_time_ix] = t + self.server_service_exp.random(0)
                 n_live_fires -= 1
 
         A_hour = ["{} day {:d}:{:d}:{:d}".format(*self.timefseconds(sec)) for sec in A]
@@ -110,9 +113,6 @@ class SFFireSimulation:
         return simulated_fire
 
     def simulate_only_times(self):
-        # init utility variables
-        fires_exp = Exponential(1 / 1050.73914786)
-        server_service_exp = Exponential(1 / 1050.7392199)
         # init variables
         # Time variable
         t = 0
@@ -127,19 +127,21 @@ class SFFireSimulation:
         # Event list
         Tserver = [math.inf for _ in range(self.N_servers)]
 
-        Ta = fires_exp.random()
+        u = np.random.uniform(0, 1)
+        Ta = self.fires_exp.random(u)
         while t < self.simulation_days * 86400:
             if Ta <= min(Ta, *Tserver):
                 # the next events is a fire
                 t = Ta
                 Na += 1
-                Ta = t + fires_exp.random()
+                u = np.random.uniform(0, 1)
+                Ta = t + self.fires_exp.random(u)
                 A.append(t)
                 n_live_fires += 1
                 if n_live_fires <= self.N_servers:
                     i = self.find_first_empty_server(SS)
                     SS[i] = Na
-                    Tserver[i] = t + server_service_exp.random()
+                    Tserver[i] = t + self.server_service_exp.random(0)
             else:
                 # the next events is an intervention end
                 min_time_ix, min_time = self.get_min_time_server(Tserver)
@@ -151,7 +153,7 @@ class SFFireSimulation:
                     Tserver[min_time_ix] = math.inf
                 else:
                     SS[min_time_ix] += 1
-                    Tserver[min_time_ix] = t + server_service_exp.random()
+                    Tserver[min_time_ix] = t + self.server_service_exp.random(0)
                 n_live_fires -= 1
 
         A = A[0:len(D)]

simulation_package/distribution.py
@@ -4,15 +4,21 @@ import numpy as np
 
 class Distribution(ABC):
 
-    def __init__(self):
-        pass
+    def __init__(self, varred=True):
+        self.varred = varred
 
     @abstractmethod
-    def random(self):
+    def random(self, u):
         pass
 
     def rvs(self, n):
-        return [self.random() for _ in range(0, n)]
+        u = np.random.uniform(0, 1, int(np.floor(n)))
+        first = [self.random(x) for x in list(u)]
+        if self.varred:
+            second = [self.random(1-x) for x in list(u)]
+            return first+second
+
+        return first
 
     def cdf(self, n):
         data = self.rvs(n)

simulation_package/exponential.py
@@ -1,14 +1,14 @@
 import numpy as np
-from scipy import random
 from .distribution import Distribution
 
 
 class Exponential(Distribution):
 
-    def __init__(self, l):
+    def __init__(self, l, varred=True):
+        super(Exponential, self).__init__(varred=varred)
         self.lam = l
 
     # generate a random value from an exponential distribution,
     # the formula was derived using the integral transformation technique
-    def random(self):
-        return -(1 / self.lam) * np.log(random.random())
+    def random(self, u):
+        return -(1 / self.lam) * np.log(u)

simulation_package/gamma.py
@@ -11,5 +11,5 @@ class Gamma(Distribution):
 
     # generate a random value from a gamma distribution,
     # the formula was derived using the integral transformation technique
-    def random(self):
+    def random(self, u):
         return -(1/self.lam)*np.log(np.prod(np.random.uniform(size=self.n)))

simulation_package/normal.py
@@ -6,16 +6,15 @@ import math
 
 class Normal(Distribution):
 
-    #idea buffer
-
     def __init__(self, mu=0.0, variance=1.0, t="bm", log=False):
+        super(Normal, self).__init__(varred=False)
         self.mu = mu
         self.sigma = np.sqrt(variance)
         self.technique = t
         self.log = log
         self.buffer = []
 
-    def random(self):
+    def random(self, u=0):
         z = self.general_normal()
         if self.log:
             return np.exp(z)